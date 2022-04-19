495ca6f2525ce40446897ebe743b40909d4dd705
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-19 15:57:38 +0600

    done

---
+++ largest.js
@@ -0,0 +1,12 @@
+var num1 = 10, num2 = 20, num3 = 40;
+
+if (num1 > num2 && num1 > num3) {
+    console.log("largest number is :" + num1);
+
+}
+else if (num2 > num2 && num2 > num3) {
+    console.log("largest number is: " + num2)
+}
+else {
+    console.log("largest number is :" + num3)
+}
---
+++ math.js
@@ -0,0 +1,12 @@
+// const myNumber = -5;
+// var output = Math.abs(myNumber);
+// console.log(output);
+
+const myNumber = 6.6692;
+// const output = Math.ceil(myNumber);
+// const output = Math.floor(myNumber);
+// const output = Math.round(myNumber);
+
+const output = Math.random(myNumber) * 10;
+const rounded = Math.ceil(output);
+console.log(rounded);
